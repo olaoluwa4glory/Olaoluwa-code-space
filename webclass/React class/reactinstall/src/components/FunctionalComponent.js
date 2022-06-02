1e2f8bdd11221cd87bc22f7f50754d563f8f7843
Olaoluwa4glory <samtex0942@gmail.com>
2022-06-02 13:09:53 +0100

    ReactPractice

---
+++ webclass/React class/reactinstall/src/components/FunctionalComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FunctionalComponent() {
+    const name = 'Olaoluwa'
+  return (
+    <div>
+       <h1>{name}</h1> 
+    </div>
+  )
+}
+
+export default FunctionalComponent
